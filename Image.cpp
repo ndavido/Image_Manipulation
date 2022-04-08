ee69ede7f3c7e980455f4d8b6dbf3f310feb795a
ndavido <D00239107@student.dkit.ie>
2022-04-08 16:14:26 +0100

    Created GrayScale, Red, Green and Blue filters

--- Image.cpp
+++ Image.cpp
@@ -7,11 +7,32 @@
 #include <cstring>
 #include "Image.h"
 
-
-
 bool Image::load(string filename)
 {
-
+    ifstream ifs(filename, std::ios::binary);
+    string magicNumber;
+    ifs >> magicNumber;
+    ifs >> this->w;
+    ifs >> this->h;
+    int p;
+    ifs >> p;
+    ifs.ignore(256, '\n');
+    if(p==255)
+    {
+        delete[] pixels;
+        pixels = new Rgb[w*h];
+        unsigned char pix[3];
+        for(int i = 0; i < w*h; i++)
+        {
+            ifs.read(reinterpret_cast<char *>(pix), 3);
+            this->pixels[i].r = pix[0];
+            this->pixels[i].g = pix[1];
+            this->pixels[i].b = pix[2];
+            if(i < 100) {
+            }
+        }
+        return true;
+    }
     return false;
 
 }
@@ -27,28 +48,49 @@ bool Image::savePPM(string filename)
 
 void Image::filterRed()
 {
-
+    for(int i = 0; i < w*h; i++)
+    {
+        this->pixels[i].g = 0;
+        this->pixels[i].b = 0;
+    }
 }
 void Image::filterGreen()
 {
-
+    for(int i = 0; i < w*h; i++)
+    {
+        this->pixels[i].r = 0;
+        this->pixels[i].b = 0;
+    }
 }
 void Image::filterBlue()
 {
-
+    for(int i = 0; i < w*h; i++)
+    {
+        this->pixels[i].r = 0;
+        this->pixels[i].g = 0;
+    }
 }
 void Image::greyScale()
 {
-
+    for (int i = 0; i < w*h; i++)
+    {
+        unsigned char greyscaleValue = ((this->pixels[i].r) + (this->pixels[i].g) + (this->pixels[i].b))/3;
+        this->pixels[i].r = greyscaleValue;
+        this->pixels[i].g = greyscaleValue;
+        this->pixels[i].b = greyscaleValue;
+    }
 }
 void Image::flipHorizontal()
 {
-
 
 }
 void Image::flipVertically()
 {
 
+}
+
+void Image::AdditionalFunction1()
+{
 
 }
 void Image::AdditionalFunction2()
@@ -59,10 +101,7 @@ void Image::AdditionalFunction3()
 {
 
 }
-void Image::AdditionalFunction1()
-{
 
-}
 
 /* Functions used by the GUI - DO NOT MODIFY */
 int Image::getWidth()
